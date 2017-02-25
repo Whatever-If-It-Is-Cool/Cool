58642a46ce2110589a8e3253ece242b20e9f827d
Lige Liu <liulige@iastate.edu>
2017-02-24 21:17:57 -0600

    node.js

---
+++ js/json.js
@@ -0,0 +1,15 @@
+function addClass(className, section, time, day, color) {
+
+}
+
+function deleteClass(className, section) {
+
+}
+
+function addSemester(year, semester) {
+
+}
+
+function deleteClass(yesr, semester) {
+
+}
