d3bc8ac9e8d94cd1b7fd6b3cd74024e0d7765a8b
untitled95 <yimingling@lyimingmacbook.student.iastate.edu>
2017-02-25 12:06:54 -0600

    加入了按钮放大功能

--- particles.js-master/demo/js/abe.js
+++ particles.js-master/demo/js/abe.js
@@ -1,7 +1,7 @@
-
 var buttons = new Vue({
-    el:'#buttonlist',
-    data:{
-        buttons:["A","spring 2017",3,4]
-    }
+    el: '#buttonlist',
+    data: {
+        buttons: ["fall", "spring", 3, 4]
+    },
+
 })
