ca1fd1790a040451b71afacf1debb8681aee7010
untitled95 <yimingling@lyimingmacbook.student.iastate.edu>
2017-02-25 12:57:59 -0600

    Newest version

---
+++ particles.js-master/windows/main.view.js
@@ -0,0 +1,16 @@
+const {
+    ipcRenderer
+} = require('electron');
+
+function test() {
+    console.log(ipcRenderer.sendSync('synchronous-message', 'ping')); // prints "pong"
+    //test
+    ipcRenderer.on('asynchronous-reply', (event, arg) => {
+        console.log(arg); // prints "pong"
+    })
+    ipcRenderer.send('asynchronous-message', 'ping');
+}
+
+function switchs() {
+    ipcRenderer.send('processwindow');
+}
