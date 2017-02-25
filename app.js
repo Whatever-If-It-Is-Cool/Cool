e641a137a41bbe69d47bd68f38a5c96fde205ac4
Meng Wang <water.wang@live.com>
2017-02-24 19:40:03 -0600

    update

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-function dsjaodia() {
-    document.getElementById('test').innerHTML = 'djoiajdsa';
-}
+var quill = new Quill('#editor-container', {
+    modules: {
+        toolbar: [
+            [{
+                header: [1, 2, false]
+            }],
+            ['bold', 'italic', 'underline'],
+            ['image', 'code-block']
+        ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow' // or 'bubble'
+});
