3d4276705af8689a7a40109fe764148fe34bcb99
Meng Wang <water.wang@live.com>
2017-02-24 21:16:04 -0600

    update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 var quill = new Quill('#editor-container', {
   modules: {
     toolbar: [
-      [{ header: [1, 2, false] }],
+      [{
+        header: [1, 2, false]
+      }],
       ['bold', 'italic', 'underline'],
       ['image', 'code-block']
     ]
   },
   placeholder: 'Compose an epic...',
-  theme: 'snow'  // or 'bubble'
+  theme: 'snow' // or 'bubble'
 });
---
+++ src/windows/main/main.view.js
@@ -0,0 +1,20 @@
+// var Vue = require('vue');
+
+var main = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!'
+    }
+})
+const {
+    ipcRenderer
+} = require('electron');
+
+function test() {
+    console.log(ipcRenderer.sendSync('synchronous-message', 'ping')); // prints "pong"
+
+    ipcRenderer.on('asynchronous-reply', (event, arg) => {
+        console.log(arg); // prints "pong"
+    })
+    ipcRenderer.send('asynchronous-message', 'ping');
+}
